573236872d786675e56dbfae6d0b087b40ea81b3
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-03-24 20:12:42 +0800

    initial commit

---
+++ src/AplikasiTodoList.java
@@ -0,0 +1,17 @@
+public class AplikasiTodoList {
+    public static String[] model = new String[10];
+
+    public static void main(String[] args) {
+    }
+
+
+    /**
+     * Menampilkan todo List
+     */
+    public static void showTodoList() {
+    }
+
+    public static void addTodoList() {
+    }
+
+};
